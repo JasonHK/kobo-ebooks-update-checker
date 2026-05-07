9c9b29ff75c633a31336a6e2f3c7b064ffadd966
Jason Kwok <git@jasonhk.dev>
2026-05-08 00:44:32 +0800

    chore: add more strings

--- src/locales/en-US/index.ts
+++ src/locales/en-US/index.ts
@@ -4,6 +4,7 @@ const en_US: BaseTranslation = {
     secondaryControls: {
         checkPage: "Check Update for Page",
         checkPageInProgress: "Checking Update...",
+        copyOutdated: "Copy Outdated Books",
     },
     libraryActions: {
         checkSingle: "Check Update",
@@ -16,6 +17,11 @@ const en_US: BaseTranslation = {
         preview: "Preview",
         skipped: "Skipped",
         failed: "Failed",
+    },
+    error: {
+        unlisted: "This book was unlisted, there’s no way to check update for this type of books at the moment.",
+        parsing: "Failed to parse the latest product ID, please contact the developer for further investigations.",
+        unknown: "Unknown error, please contact the developer for further investigations.",
     },
 };
 
--- src/locales/zh-TW/index.ts
+++ src/locales/zh-TW/index.ts
@@ -4,6 +4,7 @@ const zh_TW: Translation = {
     secondaryControls: {
         checkPage: "為本頁檢查更新",
         checkPageInProgress: "正在檢查更新……",
+        copyOutdated: "複製過時書籍",
     },
     libraryActions: {
         checkSingle: "檢查更新",
@@ -16,6 +17,11 @@ const zh_TW: Translation = {
         preview: "預覽",
         skipped: "已略過",
         failed: "檢查失敗",
+    },
+    error: {
+        unlisted: "該書已下架，目前尚未有方法為這類書籍檢查更新。",
+        parsing: "無法解析最新的產品編號，請聯絡開發者以進一步調查。",
+        unknown: "未知錯誤，請聯絡開發者以進一步調查。",
     },
 };
 
